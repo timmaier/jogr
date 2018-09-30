f2e489597c0306cf41eb52df3a7698f9996ea578
timmaier <timmaier02@gmail.com>
2018-09-30 22:34:32 +1000

    Icon updated

--- jogr/jogr.Android/MainActivity.cs
+++ jogr/jogr.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace jogr.Droid
 {
-    [Activity(Label = "jogr", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "jogr", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
