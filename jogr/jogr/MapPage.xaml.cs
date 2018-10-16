47a0b8bbd19cda4208ff2e8f10d19789d6129c38
user-PC\Joseph <josephwoolystone@gmail.com>
2018-10-16 14:01:56 +1000

    Displays Arbitrary Pin

--- jogr/jogr/MapPage.xaml.cs
+++ jogr/jogr/MapPage.xaml.cs
@@ -21,6 +21,24 @@ namespace jogr
 
             //Get a reference to the map component
             Map map = (Map)MyMap;
+            //Add pin to map
+            Pin myLocation = new Pin
+            {
+                Type = PinType.Place,
+                Position = GetLocation(),
+                Label = "me",
+                Address = "Brisbane"
+            };
+            map.Pins.Add(myLocation);
+
+            double lat = GetLocation().Latitude;
+            double lon = GetLocation().Longitude;
+
+            /*Xamarin.Forms.GoogleMaps.Bounds Australia = new Bounds(GetLocation(),GetLocation());
+
+            map.MoveCamera(CameraUpdateFactory.NewBounds(Australia, 30));
+            */
+            map.MoveToRegion(new MapSpan(GetLocation(), 0.1, 0.1), true);
         }
         
         //Pressed Back Button
@@ -29,6 +47,13 @@ namespace jogr
             System.Diagnostics.Debug.WriteLine("Pressed");
 
             await Navigation.PopAsync();
+        }
+
+        private Xamarin.Forms.GoogleMaps.Position GetLocation()
+        {
+            Xamarin.Forms.GoogleMaps.Position myPosition = new Xamarin.Forms.GoogleMaps.Position(-27.4698, 153.0251);
+
+            return myPosition;
         }
     }
 }
